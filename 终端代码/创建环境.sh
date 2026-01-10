e1c632898c8ba852f60cc39c2feb1d1dccaa1ba6
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-10 23:04:12 +0800

    Update 创建环境.sh

--- 终端代码/创建环境.sh
+++ 终端代码/创建环境.sh
@@ -1,1 +1,20 @@
-
+############################################################################################################# 
+# 创建scenv环境
+############################################################################################################# 
+conda activate base
+conda create -n scenv python=3.10.14 -y
+conda activate scenv
+conda install scanpy -y
+pip install ipykernel
+python -m ipykernel install --user --name=scenv --display-name "scenv"
+pip install papermill
+pip install ipython-autotime
+pip install openpyxl
+pip install igraph
+pip install leidenalg
+pip install scikit-image
+pip install scvi-tools[cuda]
+pip install harmonypy
+pip install infercnvpy
+pip install gseapy
+pip install sccoda
