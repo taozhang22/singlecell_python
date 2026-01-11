d93a6d6d7000f566885225637de0121161211fd3
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-11 23:31:56 +0800

    Update 创建分析前的代码文件夹.py

--- 分析前准备/创建分析前的代码文件夹.py
+++ 分析前准备/创建分析前的代码文件夹.py
@@ -2,7 +2,7 @@ def create_code_file(wd, dir):
     import os
     os.chdir(wd)
 
-    os.makedirs("{dir}/Script/raw")
-    os.makedirs("{dir}/Script/log")
-    os.makedirs("{dir}/Script/output")
+    os.makedirs(f"{dir}/Script/raw")
+    os.makedirs(f"{dir}/Script/log")
+    os.makedirs(f"{dir}/Script/output")
 create_code_file(wd="/home/students/zhangtao/research/singlecell/python", dir="Sister")
