0ad021b3ff982f3ee08227d01ed2a0591bf7f2a8
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-12 00:10:20 +0800

    Update 0_代码开头.py

--- base/0_代码开头.py
+++ base/0_代码开头.py
@@ -1,19 +1,17 @@
 # environmet: scenv
-def prepare_environment(dir):
-    import warnings
-    warnings.filterwarnings("ignore")
+import warnings
+warnings.filterwarnings("ignore")
 
-    import sys
-    from pathlib import Path
-    import os
-    import pandas as pd
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    import scanpy as sc
-    os.chdir("/home/students/zhangtao/research/singlecell/python/Sister")
+import sys
+from pathlib import Path
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scanpy as sc
+os.chdir("/home/students/zhangtao/research/singlecell/python/sister") # 需要根据实际的工作路径修改
 
-    print(f"Current python version: {sys.version}")
-    !conda list
-    pd.set_option("display.width", 1000)
-prepare_environment(dir="/home/students/zhangtao/research/singlecell/python/Sister")
+print(f"Current python version: {sys.version}")
+!conda list
+pd.set_option("display.width", 1000)
