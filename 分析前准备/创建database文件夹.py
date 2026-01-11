e95267853d9aa32d58da4a5916a3d954f9ea3b02
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-11 23:25:40 +0800

    Update 创建database文件夹.py

--- 分析前准备/创建database文件夹.py
+++ 分析前准备/创建database文件夹.py
@@ -1,1 +1,8 @@
+def create_code_file(wd, dir):
+    import os
+    os.chdir(wd)
 
+    os.makedirs("{dir}/raw")
+    os.makedirs("{dir}/log")
+    os.makedirs("{dir}/output")
+create_code_file(wd="/home/students/zhangtao/research/singlecell", dir="database")
