57fa536f850790a634417985a27057bc13689345
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-11 23:40:49 +0800

    Update 1_读取文件.py

--- base/1_读取文件.py
+++ base/1_读取文件.py
@@ -7,4 +7,4 @@ def read_files(filename, pattern):
     adata = sc.concat([sc.read_h5ad(f) for f in files])
 
     return adata
-adata = read_files(filename="/home/students/zhangtao/research/singlecell/database", pattern="*(韩国)_2.h5ad")
+adata = read_files(filename="/home/students/zhangtao/research/singlecell/database", pattern="*(韩国).h5ad")
