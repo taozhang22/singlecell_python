589efee0fd4432334abf9dff83f60174d27b4fb9
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-12 00:15:31 +0800

    Update 后台运行代码.sh

--- 终端代码/后台运行代码.sh
+++ 终端代码/后台运行代码.sh
@@ -1,5 +1,4 @@
 conda activate scenv
-cd /home/students/zhangtao/research/singlecell/database
 nohup papermill "GSE200997(印第安纳大学)/GSE200997(印第安纳大学).ipynb" \
   "output/GSE200997(印第安纳大学)_output.ipynb" \
   -k scenv \
