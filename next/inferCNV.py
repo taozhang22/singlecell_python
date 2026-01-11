767cf2873195fe487fd138c7b142b4dfd5dc88a0
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-11 20:12:20 +0800

    Update inferCNV.py

--- next/inferCNV.py
+++ next/inferCNV.py
@@ -1,7 +1,7 @@
 ############################################################################################################# 
 # 将数据进行分簇，然后绘制出umap图
 ############################################################################################################# 
-def multi_leiden_dotplot_umapplot(adata, res_list, dotplot):
+def multi_leiden_umapplot(adata, res_list, dotplot):
     import scanpy as sc
 
     for res in res_list:
@@ -9,4 +9,3 @@ def multi_leiden_dotplot_umapplot(adata, res_list, dotplot):
     sc.pl.umap(adata=adata, color=[f"leiden_{res:.2f}" for res in res_list], legend_loc="on data")
 
     return adata
-  
