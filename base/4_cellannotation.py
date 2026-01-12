54f6d94d434590eeabe5b1b128d286cf8d431ae1
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-12 09:25:56 +0800

    Update 4_cellannotation.py

--- base/4_cellannotation.py
+++ base/4_cellannotation.py
@@ -53,34 +53,37 @@ def deg(adata, my_res, outdir):
     return adata
 adata = deg(adata=adata, my_res=my_res, outdir=dir)
 
+############################################################################################################# 
+# 参数
+#############################################################################################################
+# 细胞注释阶段用到的全局变量，根据研究的实际情况进行修改
+# 强烈注意，每跑一次都要修改
+ct_map = {
+    "0": "Epithelial",
+    "1": "T_NK",
+    "2": "T_NK",
+    "3": "McDC",
+    "4": "Epithelial",
+    "5": "B_cell",
+    "6": "Fibroblast",
+    "7": "Plasma_B",
+    "8": "Fibroblast",
+    "9": "Endothelial",
+    "10": "Plasma_B",
+    "11": "Plasma_B",
+    "12": "SMC",
+    "13": "Enteric_glial",
+    "14": "MAST",
+    "15": "pDC",
+    "16": "Endothelial",
+    "17": "Neutrophil",
+}
+
+############################################################################################################# 
+# 注释，画出注释完毕的气泡图和umap图
+############################################################################################################# 
 # 将细胞进行注释
-def celltype_annotation(
-    adata, my_res, legend_fontsize=None, outdir="result/base/",
-    ct_map={
-        "0": "Epithelial",
-        "1": "Plasma_B",
-        "2": "T_NK",
-        "3": "McDC",
-        "4": "B_cell",
-        "5": "Plasma_B",
-        "6": "T_NK",
-        "7": "Fibroblast",
-        "8": "MAST",
-        "9": "Enteric_glial",
-        "10": "Endothelial"},
-    markers={
-        "B_cell": ["CD79A", "MS4A1", "BANK1"],
-        "Endothelial": ["PLVAP", "VWF"],
-        "Enteric_glial": ["PLP1", "CLU", "S100B"],
-        "Epithelial": ["KRT18", "KRT8", "EPCAM"],
-        "Fibroblast": ["COL1A1", "DCN", "COL3A1"],
-        "MAST": ["MS4A2", "KIT", "TPSAB1"],
-        "McDC": ["LYZ", "CD68", "CD14"],
-        "Neutrophil": ["G0S2", "FCGR3B", "CSF3R"],
-        "pDC": ["LILRA4", "IL3RA", "CLEC4C"],
-        "Plasma_B": ["MZB1", "DERL3", "IGHG2"],
-        "T_NK": ["CD3D", "CD3E", "KLRB1"]},
-):
+def celltype_annotation(adata, my_res, legend_fontsize, outdir, ct_map, markers):
     import scanpy as sc
     import matplotlib.pyplot as plt
 
@@ -97,11 +100,12 @@ def celltype_annotation(
     plt.show(); plt.close()
 
     # 绘制注释后的umap图
-    fig = sc.pl.embedding(adata, basis="umap_after", color="Celltype", legend_loc="on data", show=False, return_fig=True)
+    fig = sc.pl.umap(adata, color="Celltype", legend_loc="on data", show=False, return_fig=True, legend_fontsize=legend_fontsize)
     fig.savefig(f"{outdir}/pdf_umap_after_harmony_with_celltype.pdf", bbox_inches="tight")
     plt.show(); plt.close(fig)
 
     return adata
+adata = celltype_annotation(adata=adata, my_res=my_res, legend_fontsize=None, outdir=dir, ct_map=ct_map, markers=markers)
 
 
 
@@ -116,27 +120,3 @@ def adata_all_counts(adata,
     adata.obs["Celltype"] = obs_df
     adata.write_h5ad(outdir_adata_include_all_counts)
 
-#####################################################使用案例#################################################################
-# 根据情况更改项目参数
-res_list = np.arange(0.1, 1.1, 0.1)
-markers = {
-    "B_cell": ["CD79A", "MS4A1", "BANK1"],
-    "Endothelial": ["PLVAP", "VWF", "CLDN5"],
-    "Enteric_glial": ["PLP1", "CLU", "S100B"],
-    "Epithelial": ["KRT18", "KRT8", "EPCAM"],
-    "Fibroblast": ["COL1A1", "DCN", "COL3A1"],
-    "MAST": ["MS4A2", "KIT", "TPSAB1"],
-    "McDC": ["LYZ", "CD68", "CD14"],
-    "Neutrophil": ["G0S2", "FCGR3B", "CSF3R"],
-    "pDC": ["LILRA4", "IL3RA", "CLEC4C"],
-    "Pericyte": ["MCAM", "PDGFRB", "CSPG4"],
-    "Plasma_B": ["MZB1", "DERL3", "IGHG2"],
-    "SMC": ["CNN1", "TAGLN", "DES"],  # Smooth_muscle_cell
-    "T_NK": ["CD3D", "CD3E", "KLRB1"],
-}
-adata = scell.base.multi_leiden(adata, res_list=res_list, markers=markers)
-adata.write_h5ad(f"{dir}/adata_leiden.h5ad")
-
-# 暂停，根据情况更改项目参数
-my_res = 0.3
-adata = scell.base.deg(adata, my_res, outdir=dir)
