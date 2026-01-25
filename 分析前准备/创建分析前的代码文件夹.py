846dd9cf74f5c528501fb5a7079c62a15bddb9eb
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-25 17:16:13 +0800

    Delete 分析前准备/创建分析前的代码文件夹.py

--- 分析前准备/创建分析前的代码文件夹.py
+++
@@ -1,8 +0,0 @@
-def create_code_file(wd, dir):
-    import os
-    os.chdir(wd)
-
-    os.makedirs(f"{dir}/Script/raw", exist_ok=True)
-    os.makedirs(f"{dir}/Script/log", exist_ok=True)
-    os.makedirs(f"{dir}/Script/output", exist_ok=True)
-create_code_file(wd="/home/students/zhangtao/research/singlecell/python", dir="sister")
