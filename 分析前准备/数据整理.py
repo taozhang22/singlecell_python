45cd1945072f1c1071fcb0daff102c8d2075e0b0
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-25 22:22:16 +0800

    Update 数据整理.py

--- 分析前准备/数据整理.py
+++ 分析前准备/数据整理.py
@@ -34,4 +34,22 @@ adata.obs = meta.copy()
 adata.write_h5ad(f"{dir}/python/{dir}.h5ad")
 adata.obs["Sample"].value_counts()
 
+# 质量控制
+adata.var["mt"] = adata.var_names.str.startswith("MT-")
+adata.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL"))
+adata.var["hb"] = adata.var_names.str.contains("^HB[^(P)]")
+sc.pp.calculate_qc_metrics(adata, qc_vars=["mt", "ribo", "hb"], inplace=True, percent_top=None, log1p=False)
+
+for Sample in adata.obs["Sample"].astype(str).unique().tolist():
+    fig, axes = plt.subplots(1, 2, figsize=(6, 2.5))
+    sc.pl.scatter(adata[adata.obs['Sample'] == Sample], x="total_counts", y="pct_counts_mt", ax=axes[0], show=False)
+    sc.pl.scatter(adata[adata.obs['Sample'] == Sample], x="total_counts", y="n_genes_by_counts", ax=axes[1], show=False)
+    plt.tight_layout(); plt.show(); plt.close(fig)
+    
+fig, axes = plt.subplots(5, 1, figsize=(0.3 * adata.obs["Sample"].nunique(), 15))
+for i, key in enumerate(["n_genes_by_counts", "total_counts", "pct_counts_mt", "pct_counts_ribo", "pct_counts_hb"]):
+    sc.pl.violin(adata, keys=key, groupby="Sample", jitter=0.4, rotation=45, show=False, ax=axes[i])
+plt.tight_layout(); plt.show(); plt.close(fig)
+
+adata.obs["Sample"].value_counts()
 
