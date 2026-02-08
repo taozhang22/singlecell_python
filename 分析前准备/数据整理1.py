4674ccc26f725b084ef61ee92026516fcce85d5a
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-08 16:47:05 +0800

    Update 数据整理1.py

--- 分析前准备/数据整理1.py
+++ 分析前准备/数据整理1.py
@@ -8,7 +8,7 @@ import pandas as pd
 import datatable as dt
 import matplotlib.pyplot as plt
 import scanpy as sc
-os.chdir("/home/yang/research/bioinformation/singlecell/database") # 根据实际工作路径进行修改
+os.chdir("/media/yang/9EBA46B1BA46862D/research/bioinformation/singlecell/database/") # 根据实际工作路径进行修改
 
 # 以下参数根据实际情况进行修改
 dir = "GSE132465"
